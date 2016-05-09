499ca4f6211145c2c09c0a3ee37ec37d50ec24d2
ppayter-cc <pomsarpeter@gmail.com>
2016-05-09 11:44:51 +0200

    Add files via upload

---
+++ 100doors.py
@@ -0,0 +1,16 @@
+hallway = [0]*100
+step = 1
+
+while step <= len(hallway):
+    door = step -1
+    while door < len(hallway):
+        if hallway[door] == 0:
+            hallway[door] = 1
+        else:
+            hallway[door] = 0
+        door = door + step
+    step = step + 1
+
+for door in range (0, len(hallway)):
+    if hallway[door] == 1:
+        print (door+1)
